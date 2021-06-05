eb7d5108e7fe0d9ca6f9c434597c9588d8351b9b
lifeistech <happymmw5885@gmail.com>
2021-06-05 15:25:42 +0900

    ここまでか...っ！！

--- app/src/main/java/com/example/checkactivity/MainActivity.kt
+++ app/src/main/java/com/example/checkactivity/MainActivity.kt
@@ -13,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         val textView:TextView = findViewById(R.id.textView)
         val button: Button = findViewById(R.id.button)
         var i:Int = 0
+        val stra:String = "Life is Tech!"
+        val strb:String = "名古屋土曜"
+        val strc:String = "Androidコース"
+        val strd:String = "あみだ班"
+
 
         button.setOnClickListener {
+            for(){
             i++
             textView.text = (i.toString())
+
 
         }
     }
