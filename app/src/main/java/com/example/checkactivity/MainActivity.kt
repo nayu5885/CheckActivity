5a7b1f0dcee5436c26bac499718bb76b7db1bac5
lifeistech <happymmw5885@gmail.com>
2021-06-05 15:04:50 +0900

    カウント機能をつけた

--- app/src/main/java/com/example/checkactivity/MainActivity.kt
+++ app/src/main/java/com/example/checkactivity/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.checkactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView:TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+        var i:Int = 0
+
+        button.setOnClickListener {
+            i++
+            textView.text = (i.toString())
+
+        }
     }
+
+
 }
